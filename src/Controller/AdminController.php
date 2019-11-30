9ade5617f34c3c31a465b19d57120fac4371c653
john43 <akpakienockjohn43@gmail.com>
2019-11-30 13:05:26 +0100

    Debut du projet

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index()
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+    /**
+     * @Route("/", name="admin")
+     */
+    public function idex()
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+
+    /**
+     * @Route("/admin/liste", name="liste")
+     */
+    public function liste(){
+        return $this -> render ('admin/liste-clients.html.twig');
+    }
+}
