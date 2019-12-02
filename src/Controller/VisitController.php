3e1927d2b9597c22b8cfa8d8c60e1ef5871ae3cd
john43 <akpakienockjohn43@gmail.com>
2019-12-02 14:15:36 +0100

    modification finale de la page properties

--- src/Controller/VisitController.php
+++ src/Controller/VisitController.php
@@ -21,4 +21,25 @@ class VisitController extends AbstractController
     {
         return $this->render('visit/properties.html.twig');
     }
+    /**
+     * @Route("/view", name="view")
+     */
+    public function view()
+    {
+        return $this->render('visit/view.html.twig');
+    }
+    /**
+     * @Route("/request", name="request")
+     */
+    public function request()
+    {
+        return $this->render('request/request.html.twig');
+    }
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact()
+    {
+        return $this->render('visit/contact.html.twig');
+    }
 }
