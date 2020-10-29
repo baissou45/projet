1c99d3a4294204e74857124654a8f944d8a40be9
baissou45 <tbaissou@gmail.com>
2020-10-29 12:45:59 +0100

    Reglage de la premiere page de l'admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -7,17 +7,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
-    /**
-     * @Route("/admin", name="admin")
-     */
-    public function index()
-    {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
-        ]);
     
-    }
-
     /**
      * @Route("/admin", name="admin")
      */
